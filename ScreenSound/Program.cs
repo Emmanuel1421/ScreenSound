0f676893656d00015ec55b85df93a4ee27efc8aa
Emmanuel Guerra <emmanuelguerra1945@gmail.com>
2026-02-24 15:01:21 -0300

    Add files via upload

---
+++ ScreenSound/Program.cs
@@ -0,0 +1,35 @@
+﻿using ScreenSound;
+
+/*
+var cb = new ContaBancaria();
+cb.MostrarInformacoes();
+var v = new Veiculo();
+v.MenuAcao();
+v.Acelerar();
+v.Frear();
+v.Buzinar();
+*/
+
+var song = new List<Musica>();
+
+var m1 = new Musica();
+m1.Disponivel = true;
+Console.WriteLine(m1.Disponivel);
+m1.Nome = "Numb";
+m1.Artista = "Linkin Park";
+m1.Duracao = 350;
+//m1.InserirMusica();
+m1.ExibirFichaTecnica();
+
+var m2 = new Musica();
+m2.Disponivel = false;
+Console.WriteLine(m2.Disponivel);
+m2.Nome = "The trooper";
+m2.Artista = "Iron Maiden";
+m2.Duracao = 280;
+//m2.InserirMusica();
+m2.ExibirFichaTecnica();
+
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ ScreenSound/ScreenSound.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound
+{
+    class ContaBancaria
+    {
+        public string id, titular;
+        public double saldo;
+        public int senha;
+
+        
+
+        public void MostrarInformacoes() 
+        {
+            this.saldo = 100.00;
+            this.titular = "Emmanuel";
+
+            Console.WriteLine($"O titular da conta é {this.titular} e o saldo é {this.saldo}");
+        }
+    }
+    class Veiculo 
+    {
+
+        double velocidade = 0.5;
+        double aceleracao = 0.1;
+        
+        int resposta;
+        
+
+        
+        public void MenuAcao() 
+        {
+            Console.WriteLine("Digite a opção desejada:\n1.Acelerar \n2.Frear \n3.Buzinar \n");
+            resposta = int.Parse(Console.ReadLine());
+            switch (resposta)
+            {
+                case 1:
+                    Acelerar();
+                    break;
+                case 2:
+                    Frear();
+                    break;
+                case 3:
+                    Buzinar();
+                    break;
+                default:
+                    Console.WriteLine("Selecione uma opção válida.");
+                    break;
+            }
+
+            }
+        
+
+        public void Acelerar() 
+        {
+            if(velocidade == 0) 
+            { velocidade++; }
+            for (aceleracao = 1.0;aceleracao < 2.0; velocidade *= aceleracao) 
+            {
+                
+                aceleracao++;
+                Console.WriteLine($"{velocidade}Km/h");
+                
+            }
+            MenuAcao();
+        }
+
+        public void Frear()
+        {
+            if (velocidade <= 0)
+            {
+                return;
+            }
+            double deceleration = 3.0;
+            while (velocidade > 0)
+            {
+                velocidade = Math.Max(0, velocidade - deceleration);
+                Console.WriteLine($"{velocidade} Km/h");
+            }
+            MenuAcao();
+        }
+
+        public void Buzinar() 
+        {
+            Console.WriteLine("bi-bi");
+            MenuAcao();
+        }
+    }
+}
+
+
